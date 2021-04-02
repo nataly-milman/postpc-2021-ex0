018ca2394eb36c913e937cfc54a10d4c99011dd3
nataly-milman <63509956+nataly-milman@users.noreply.github.com>
2021-04-02 13:13:33 +0300

    first commit

---
+++ app/src/main/java/huji/postpc/nataly/milman/MainActivity.kt
@@ -0,0 +1,11 @@
+package huji.postpc.nataly.milman
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
